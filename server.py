70147272ea2bea445762d321d68822d22309d654
Bingqing Wei <bw2581@columbia.edu>
2019-04-03 14:03:18 -0400

    events added

--- server.py
+++ server.py
@@ -22,6 +22,9 @@ login_manager.login_view = "login"
 def time_now():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+def str2datetime(s):
+    return datetime.datetime.strptime(s, "%Y-%m-%d %H:%M:%S")
+
 def double_quote(s):
     return "'" + s + "'"
 
@@ -38,7 +41,7 @@ class User(flask_login.UserMixin):
 
 @login_manager.user_loader
 def user_loader(user_id):
-    cursor = g.conn.execute("SELECT * FROM users WHERE user_id={}".format(user_id))
+    cursor = g.conn.execute("SELECT * FROM users WHERE user_id={};".format(user_id))
     row = cursor.fetchone()
     if row:
         return User(row['user_id'], row['name'])
@@ -138,7 +141,7 @@ def index():
 
 @app.route('/world', methods=['GET'])
 def world():
-    cursor = g.conn.execute("SELECT c_id, image, text, likes FROM content")
+    cursor = g.conn.execute("SELECT c_id, image, text, likes FROM content;")
     content = {}
     for result in cursor:
         content[result['c_id']] = [result['image'], result['text'], result['likes']]
@@ -148,12 +151,26 @@ def world():
     return render_template('world.html', **context)
 
 
+def get_categories(user_id):
+    scmd_01 = "SELECT C.c_id as c_id, C.interest_area as interest_area \n" \
+              "FROM category as C, users as U, belongto_relation as B \n" \
+              "WHERE U.user_id=B.user_id AND C.c_id=B.c_id AND U.user_id={};".format(user_id)
+    cursor = g.conn.execute(scmd_01)
+    category = []
+    for result in cursor:
+        dic = dict()
+        dic['c_id'] = result['c_id']
+        dic['interest_area'] = result['interest_area']
+        category.append(dic)
+    cursor.close()
+    return category
+
 @app.route('/show/<c_id>', methods=['GET'])
 def show(c_id):
     scmd_0 = "SELECT C.c_id as c_id, U.user_id as user_id, U.name as name, " \
              "C.image as image, C.text as text, C.likes as likes \n" \
              "FROM content as C, users as U, postcontent_relation as P " \
-             "WHERE C.c_id = {} AND U.user_id=P.user_id AND P.content_id=C.c_id".format(c_id)
+             "WHERE C.c_id = {} AND U.user_id=P.user_id AND P.content_id=C.c_id;".format(c_id)
 
     cursor = g.conn.execute(scmd_0)
     row = cursor.fetchone()
@@ -168,7 +185,7 @@ def show(c_id):
     content['name'] = row['name']
     cursor.close()
 
-    cursor = g.conn.execute('SELECT * FROM follow_relation F WHERE F.follower_id=%s AND F.following_id=%s',
+    cursor = g.conn.execute('SELECT * FROM follow_relation F WHERE F.follower_id=%s AND F.following_id=%s;',
                             (flask_login.current_user.id, content['user_id']))
     row = cursor.fetchone()
     if row or content['user_id'] == flask_login.current_user.id:
@@ -179,7 +196,7 @@ def show(c_id):
     scmd_1 = "SELECT R.time as time, C.text as text, U.name as name, U.user_id as user_id \n" \
              "FROM comments as C, commentat_relation as R, postcomment_relation as P, users as U \n" \
              "WHERE C.c_id=R.comment_id AND R.content_id={} AND U.user_id=P.user_id \n" \
-             "AND P.comment_id=C.c_id".format(c_id)
+             "AND P.comment_id=C.c_id;".format(c_id)
     cursor = g.conn.execute(scmd_1)
     comments = []
     for result in cursor:
@@ -192,29 +209,15 @@ def show(c_id):
 
     context = dict(data=content)
     context['comments'] = comments
-    scmd_01 = "SELECT C.c_id as c_id, C.interest_area as interest_area \n" \
-              "FROM category as C, users as U, belongto_relation as B \n" \
-              "WHERE U.user_id=B.user_id AND C.c_id=B.c_id AND U.user_id={}".format(content['user_id'])
-    cursor = g.conn.execute(scmd_01)
-    catagory = []
-    for result in cursor:
-        dic = dict()
-        dic['c_id'] = result['c_id']
-        dic['interest_area'] = result['interest_area']
-        catagory.append(dic)
-    cursor.close()
-    context['catagory'] = catagory
+    context['catagory'] = get_categories(content['user_id'])
     cursor.close()
     return render_template('show.html', **context)
 
-'''
-获取当前用户的catagory
-'''
 @app.route('/profile', methods=['GET'])
 def profile():
     sql_cmd = "SELECT C.c_id as c_id, C.likes as likes, C.image as image, C.text as text\n" \
               "FROM postcontent_relation as P, content as C\n" \
-              "WHERE P.content_id = C.c_id AND P.user_id={}".format(flask_login.current_user.id)
+              "WHERE P.content_id = C.c_id AND P.user_id={};".format(flask_login.current_user.id)
     cursor = g.conn.execute(sql_cmd)
     contents = []
     for result in cursor:
@@ -229,7 +232,7 @@ def profile():
 
     scmd_01 = "SELECT C.c_id as c_id, C.interest_area as interest_area \n" \
               "FROM category as C, users as U, belongto_relation as B \n" \
-              "WHERE U.user_id=B.user_id AND C.c_id=B.c_id AND U.user_id={}".format(flask_login.current_user.id)
+              "WHERE U.user_id=B.user_id AND C.c_id=B.c_id AND U.user_id={};".format(flask_login.current_user.id)
     cursor = g.conn.execute(scmd_01)
     catagory = []
     for result in cursor:
@@ -247,7 +250,7 @@ def following():
     sql_cmd = "SELECT C.c_id as content_id, C.likes as likes, C.image as image, C.text as text, P.user_id as user_id, U.name as name \n" \
               "FROM follow_relation as F, content as C, postcontent_relation as P, users as U \n" \
               "WHERE F.follower_id={} AND P.content_id=C.c_id AND P.user_id=F.following_id " \
-              "AND U.user_id=F.following_id".format(flask_login.current_user.id)
+              "AND U.user_id=F.following_id;".format(flask_login.current_user.id)
 
     cursor = g.conn.execute(sql_cmd)
     fs = dict()
@@ -256,22 +259,128 @@ def following():
         if following_id not in fs:
             fs[following_id] = []
         content = dict()
-        # TODO 1. get the catagory of the following
         content['name'] = result['name']
         content['c_id'] = result['content_id']
         content['likes'] = result['likes']
         content['image'] = result['image']
         content['text'] = result['text']
+        content['category'] = get_categories(following_id)
         fs[following_id].append(content)
     cursor.close()
     context = dict(data=fs)
 
     return render_template('following.html', **context)
 
-# TODO 1. get event information  2. people who attend this event
-@app.route('event', methods=['GET'])
+'''
+TODO administor delete an event
+'''
+def admin_del_event(e_id):
+    """
+    only allow administrator to delete an event published by him/her
+    enforced by: e_id
+
+    :param e_id:
+    :return:
+    """
+    scmd_1 = "DELETE FROM event WHERE e_id={};".format(e_id)
+    g.conn.execute(scmd_1)
+
+
+'''
+TODO administrator login page
+see all published events
+'''
+def event_by_admin(admin_id):
+    scmd_1 = "SELECT E.e_id as e_id, A.addr_id as addr_id, " \
+             "A.city as city, A.street as street, A.zipcode as zipcode," \
+             "E.start_time as start_time, E.end_time = E.end_time\n" \
+             "FROM event as E, at_relation as R, address as A, admin as D\n" \
+             "WHERE E.e_id=R.e_id AND A.addr_id=R.addr_id AND D.admin_id={};".format(admin_id)
+    cursor = g.conn.execute(scmd_1)
+    events = []
+    for result in cursor:
+        content = dict()
+        content['e_id'] = result['e_id']
+        content['addr_id'] = result['addr_id']
+        content['city'] = result['city']
+        content['street'] = result['streeet']
+        content['zipcode'] = result['zipcode']
+        content['start_time'] = result['start_time']
+        content['end_time'] = result['end_time']
+        events.append(content)
+    cursor.close()
+
+"""
+get all events registered by user
+"""
+def event_by_user(user_id):
+    scmd_1 = "SELECT E.e_id as e_id, A.addr_id as addr_id, G.time as time, " \
+             "A.city as city, A.street as street, A.zipcode as zipcode," \
+             "E.start_time as start_time, E.end_time = E.end_time\n" \
+             "FROM event as E, at_relation as R, address as A, users as U, register_relation as G\n" \
+             "WHERE E.e_id=R.e_id AND A.addr_id=R.addr_id AND U.user_id={} " \
+             "AND G.user_id=U.user_id AND G.event_id=E.e_id;".format(user_id)
+    cursor = g.conn.execute(scmd_1)
+    events = []
+    for result in cursor:
+        content = dict()
+        content['e_id'] = result['e_id']
+        content['addr_id'] = result['addr_id']
+        content['city'] = result['city']
+        content['street'] = result['streeet']
+        content['zipcode'] = result['zipcode']
+        content['start_time'] = result['start_time']
+        content['end_time'] = result['end_time']
+        content['register_time'] = result['time']
+        events.append(content)
+    cursor.close()
+
+
+def register_event(e_id, user_id, end_time):
+    """
+    fecthes start_time and end_time from event table first
+    check if now() < event.end_time
+    :param e_id:
+    :param user_id:
+    :param end_time: get it from the event html
+    :return: True if successfully registered, False if not successful
+    """
+    now = datetime.datetime.now()
+    end_time = str2datetime(end_time)
+    if now >= end_time:
+        return False
+
+
+    scmd_1 = "INSERT INTO register_relation(user_id, event_id) " \
+             "VALUES ({}, {});".format(user_id, e_id)
+    g.conn.execute(scmd_1)
+    return True
+
+
+@app.route('/event', methods=['GET'])
 def event():
-    pass
+    """
+    get all events
+    """
+    scmd_1 = "SELECT E.e_id as e_id, A.addr_id as addr_id, " \
+             "A.city as city, A.street as street, A.zipcode as zipcode," \
+             "E.start_time as start_time, E.end_time = E.end_time\n" \
+             "FROM event as E, at_relation as R, address as A\n" \
+             "WHERE E.e_id=R.e_id AND A.addr_id=R.addr_id;"
+    cursor = g.conn.execute(scmd_1)
+    events = []
+    for result in cursor:
+        content = dict()
+        content['e_id'] = result['e_id']
+        content['addr_id'] = result['addr_id']
+        content['city'] = result['city']
+        content['street'] = result['streeet']
+        content['zipcode'] = result['zipcode']
+        content['start_time'] = result['start_time']
+        content['end_time'] = result['end_time']
+        events.append(content)
+    cursor.close()
+
 
 @app.route('/addComment', methods=['POST'])
 def addComment():
@@ -279,7 +388,7 @@ def addComment():
     text = double_quote(text)
     content_id = request.form.get('content_id')
 
-    scmd_1 = "INSERT INTO comments(text) VALUES ({}) RETURNING c_id".format(text)
+    scmd_1 = "INSERT INTO comments(text) VALUES ({}) RETURNING c_id;".format(text)
 
     cursor = g.conn.execute(scmd_1)
     row = cursor.fetchone()
@@ -287,9 +396,9 @@ def addComment():
         raise SystemError('Error in inserting into comments table')
     cursor.close()
     c_id = row['c_id']
-    scmd_1_del = "DELETE FROM comments WHERE c_id={}".format(c_id)
+    scmd_1_del = "DELETE FROM comments WHERE c_id={};".format(c_id)
     scmd_2 = "INSERT INTO postcomment_relation(user_id, comment_id) " \
-             "VALUES ({}, {})".format(flask_login.current_user.id, c_id)
+             "VALUES ({}, {});".format(flask_login.current_user.id, c_id)
     try:
         g.conn.execute(scmd_2)
     except Exception as ex:
@@ -298,9 +407,9 @@ def addComment():
 
 
     scmd_2_del = "DELETE FROM postcomment_relation " \
-                 "WHERE user_id={}, comment_id={}".format(flask_login.current_user.id, c_id)
+                 "WHERE user_id={}, comment_id={};".format(flask_login.current_user.id, c_id)
     scmd_3 = "INSERT INTO commentat_relation(comment_id, content_id, time)" \
-             "VALUES ({}, {}, {})".format(c_id, content_id, double_quote(time_now()))
+             "VALUES ({}, {}, {});".format(c_id, content_id, double_quote(time_now()))
     try:
         g.conn.execute(scmd_3)
     except Exception as ex:
@@ -311,14 +420,13 @@ def addComment():
     return redirect(url_for('show', c_id = content_id))
 
 
-
 @app.route('/addContent', methods=['POST'])
 def addContent():
     image = request.form.get('image')
     image = double_quote(image)
     text = double_quote(request.form.get('text'))
     scmd_1 = "INSERT INTO content(image, text) " \
-             "VALUES ({}, {}) RETURNING c_id".format(image, text)
+             "VALUES ({}, {}) RETURNING c_id;".format(image, text)
     cursor = g.conn.execute(scmd_1)
     row = cursor.fetchone()
     if not row: raise SystemError('Error in inserting into content table')
@@ -326,7 +434,7 @@ def addContent():
     cursor.close()
     scmd_1_del = "DELETE FROM content WHERE c_id={}".format(c_id)
     scmd_2 = "INSERT INTO postcontent_relation(content_id, user_id, time) " \
-             "VALUES ({}, {}, {})".format(c_id, flask_login.current_user.id, double_quote(time_now()))
+             "VALUES ({}, {}, {});".format(c_id, flask_login.current_user.id, double_quote(time_now()))
 
     try:
         g.conn.execute(scmd_2)
@@ -336,13 +444,16 @@ def addContent():
 
     return redirect(url_for('profile'))
 
+
+
 @app.route('/addFollowing', methods=['POST'])
 def addFollowing():
     following_id = request.form.get('user_id')
     scmd_1 = "INSERT INTO follow_relation(following_id, follower_id) " \
-             "VALUES ({}, {})".format(following_id, flask_login.current_user.id)
+             "VALUES ({}, {});".format(following_id, flask_login.current_user.id)
     g.conn.execute(scmd_1)
     return redirect(url_for('following'))
+
 
 
 @app.route('/deleteContent', methods=['POST'])
@@ -354,20 +465,24 @@ def deleteContent():
     scmd_02 = "DELETE FROM comments \n" \
               "WHERE c_id in (SELECT C.c_id \n" \
                              "FROM comments as C, commentat_relation as P \n" \
-                              "WHERE C.c_id=P.comment_id AND P.content_id={})".format(content_id)
+                              "WHERE C.c_id=P.comment_id AND P.content_id={});".format(content_id)
 
-    scmd_01 = "DELETE FROM content WHERE c_id={}".format(content_id)
+    scmd_01 = "DELETE FROM content WHERE c_id={};".format(content_id)
     g.conn.execute(scmd_02)
     g.conn.execute(scmd_01)
     return redirect(url_for('profile'))
+
+
 
 @app.route('/deleteFollowing', methods=['POST'])
 def deleteFollowing():
     following_id = request.form.get('user_id')
     scmd_01 = "DELETE FROM follow_relation " \
-              "WHERE follower_id={} and following_id={}".format(flask_login.current_user.id, following_id)
+              "WHERE follower_id={} and following_id={};".format(flask_login.current_user.id, following_id)
     g.conn.execute(scmd_01)
     return redirect(url_for('following'))
+
+
 
 @app.route('/modifyCatagory', methods=['POST'])
 def modifyCatagory():
@@ -376,8 +491,19 @@ def modifyCatagory():
     selectedCatagory = temp_dict[0][1]
     print(selectedCatagory)
 
-    #TODO 1. delete previous belongToCatagory 2. add these new catagory
+    user_id = flask_login.current_user.id
+    scmd_01 = "DELETE FROM belongto_relation " \
+              "WHERE user_id={};".format(user_id)
+    g.conn.execute(scmd_01)
+
+
+    values = ['({}, {})'.format(user_id, c_id) for c_id in selectedCatagory]
+    scmd_02 = "INSERT INTO belongto_relation(user_id, c_id) " \
+              "VALUES {};".format(', '.join(values))
+    g.conn.execute(scmd_02)
     return redirect(url_for('profile'))
+
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -389,7 +515,7 @@ def register():
         try:
             # use this to get auto-incremented primary key value
             cursor = g.conn.execute("INSERT INTO users(date_of_birth, pwd, name) \n"
-                                    "VALUES (%s, %s, %s) RETURNING user_id", (dateOfBirth, password, username))
+                                    "VALUES (%s, %s, %s) RETURNING user_id;", (dateOfBirth, password, username))
             row = cursor.fetchone()
             if not row: raise SystemError('Insertion failed')
             print('Created user with id {}'.format(row['user_id']))
@@ -404,6 +530,8 @@ def register():
         return render_template('register.html')
 
 
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -411,7 +539,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        cursor = g.conn.execute("SELECT * FROM users WHERE name=%s AND pwd=%s", (username, password))
+        cursor = g.conn.execute("SELECT * FROM users WHERE name=%s AND pwd=%s;", (username, password))
 
         row = cursor.fetchone()
         cursor.close()
@@ -432,12 +560,16 @@ def login():
         return render_template("login.html")
 
 
+
+
 @app.route('/logout', methods=['GET'])
 @flask_login.login_required
 def logout():
     flask_login.logout_user()
 
     return redirect(url_for('index'))
+
+
 
 
 if __name__ == "__main__":
